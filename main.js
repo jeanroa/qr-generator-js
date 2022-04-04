96ccd72bcc94a054b9f59f69627d61ccdfa043a1
jeanroa <roajeanmarco@gmail.com>
2022-04-03 21:22:59 -0400

    se agregan estilos nuevos y adaptabilidad a dispositivos moviles

--- main.js
+++ main.js
@@ -3,9 +3,16 @@ function generateQRCode() {
   if (website) {
     let qrcodeContainer = document.getElementById("qrcode");
     qrcodeContainer.innerHTML = "";
-    new QRCode(qrcodeContainer, website);
+    new QRCode(qrcodeContainer, {
+      text: website,
+      width: 150,
+      height: 150,
+      colorDark: "#1f243e",
+      colorLight: "#ffffff",
+      correctLevel: QRCode.CorrectLevel.H
+    });
     document.getElementById("qrcode-container").style.display = "block";
   } else {
-    alert("Por favor, ingresa una URL válida");
+    alert("Please enter a valid URL");
   }
 }
